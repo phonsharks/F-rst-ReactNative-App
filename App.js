94883a6ded01951e5d46e31f6853c73551282a3a
Nedim <btnedim.kahraman@gmail.com>
2022-09-23 11:36:00 +0300

    add with to do list

--- App.js
+++ App.js
@@ -4,65 +4,73 @@ import { styles } from './style';
 //import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default App=()=> {
-  //usestate bazında ikinci parametre içinde ilk parametreyi tutuyorum
-  const [text, settext] = useState("")
-  const [tasks,setTasks] = useState([])
-
-  //Var olanın içine daha sonra ekleme yapacağımdan dolayı sistem bu şekikde oldu
-  const addPress=()=>{
-    setTasks([...tasks,text]);
-    setTasks("");
-  }
-
-  const addDelete=(index)=>{
-     const newTasks=[...tasks];
-     newTasks.splice(index,1);
-     setTasks(newTasks);
-  }
+  const[todotitle,settodotitle] = useState("")
+  const [todolist,settodolist] = useState(inittodolist)
+  let inittodolist=[
+    {title:"Disiplin",complate:true},
+    {title:"Çalışma",complate:true},
+    {title:"Azim",complate:true},
+    {title:"Başar",complate:true},
+  ]
   return (
-    <SafeAreaView style={{flex:1}}>
-      <View style={styles.container}>
-        <Text style={styles.title}>To Do App</Text>
-        
-        <Text style={styles.subtitle}>Listeye veri ekleme işlemi</Text>
-
-        <TextInput 
-        style={styles.ınput} 
-        placeholder="İlgili alanı doldurunuz"
-        onChangeText={(value)=>{}}
+    <SafeAreaView 
+    style=
+    {{
+      flex:1,
+      justifyContent:"center",
+      alignItems:"center"
+    }}>
+      <View 
+      style=
+      {{flex:3,
+        alignItems:"center",
+        justifyContent:"center",
+        padding:10,width:"100%"
+      }}
         >
-        </TextInput>
+        <TextInput
+        onChangeText={(value)=>settodotitle(value)}
+        value={todotitle}
+        style=
+        {{
+          width:"100%",
+          borderColor:"#00f",
+          borderWidth:2,
+        }}
+          >
 
-        <TouchableOpacity 
-        style={styles.BtnContainer}
-        onPress={addPress}
-        >
-          <Text style={styles.btntxt}>Ekle</Text>
-        </TouchableOpacity>
-
-        <View style={styles.divider}></View>
-
-        {/*Burada flatlist etiketi liste görüntüleme de kullanılıyor*/}
-        {/* İlk prop göstermek istediğimiz data */}
-        {/* İkinci prop ise datayı nasıl göstermek istediğimiz */}
-        <FlatList 
-        data={tasks}
-        renderItem={({item,index})=>(
-          <View style={styles.taskcontainer}>
-            <Text style={styles.tasktext}></Text>
-            <TouchableOpacity 
-            style={styles.taskdelete}
-            onPress={()=>addDelete(index)}
-            >
-              <Text>X</Text>
-            </TouchableOpacity>
-          </View>
-        )}
-        keyExtractor={item=>item+Date.now()+Math.random()}
-        >
- 
-        </FlatList>
+          </TextInput>
       </View>
+
+      <View style={{flex:1}}>
+        {
+          todolist.forEach(Element=>{
+              <Text style={{}}>
+
+              </Text>
+          })
+        }
+      </View>
+
+      <TouchableOpacity 
+      style=
+      {{width:60,
+        height:60,
+        backgroundColor:"#00f",
+        alignItems:"center",
+        justifyContent:"center",
+        borderRadius:20,
+        //Burada position absolute olması konumlandırmamı manuel yaptı
+        position:'absolute',
+        right:30,
+        bottom:30
+      }}>
+        <Text 
+        style=
+        {{color:"#fff",
+        fontSize:20,
+        }}>+</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
